31a5d23e2b31aacc1e5c176e03fc9316ab2dda14
Nate <doups3@gmail.com>
2019-09-20 15:19:32 -0600

    Day 1
    n/a

---
+++ index.js
@@ -0,0 +1,18 @@
+// var obj = {
+//     key: 'val'
+// };
+// obj['key'] == obj.key;
+
+// databse.get('user')
+
+// var user = {
+//     name: 'name',
+//     age: 21,
+//     height: '5\'5"'
+// };
+
+const mustNotChange = 't';
+let thisCanChange = 't';
+
+mustNotChange = 'tt';
+thisCanChange = 'tt';
